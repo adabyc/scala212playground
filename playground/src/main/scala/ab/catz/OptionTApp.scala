37f563c7323d3f4a131c30756cad9e2de7bfd688
adabyc <adambyczek.dev@gmail.com>
2020-11-18 21:09:33 +0000

    OptionT example

---
+++ playground/src/main/scala/ab/catz/OptionTApp.scala
@@ -0,0 +1,46 @@
+package ab.catz
+
+import cats.data._
+import cats.implicits._
+import org.scalatest.Matchers
+
+import scala.util.{Success, Try}
+
+/**
+  * OptionT[F[_], A] is a light wrapper on an F[Option[A]]
+  */
+object OptionTApp extends App with Matchers {
+
+  val goodResp: Try[Option[String]] = Try(Option("200"))
+  val badResp: Try[Option[String]] = Try(Option("500"))
+
+  /**
+    * Scenario - async response has to be mapped to some other value
+    */
+
+  /** OptionT allows to map/flatMap etc over inner Effect which is Option */
+  val goodOptT: OptionT[Try, String] = OptionT(goodResp)
+  val goodDataT: OptionT[Try, Int] = goodOptT.filter(_ == "200").map(respStr => respStr.toInt)
+  goodDataT.value shouldBe Success(Option(200))
+
+  val badOptT = OptionT(badResp)
+  val badResultT: OptionT[Try, String] = badOptT.filter(_ == "200")
+  badResultT.value shouldBe Try(None)
+  val badDataT = badResultT.map(_.toInt)
+  badDataT.value shouldBe Try(None)
+
+    /**  Without Monad transformer (OptionT) explicit version is as follows */
+  val goodResult: Try[Option[String]] = goodResp.map(maybeOk => maybeOk.filter(_ == "200"))
+  goodResult shouldBe Success(Some("200"))
+
+  val goodData: Try[Option[Int]] = goodResult.map(someRes => someRes.map(_ => 200))
+  goodData shouldBe Success(Option(200))
+
+  // bad
+  val badResult = badResp.map(maybeOk => maybeOk.filter(_ == "200"))
+  badResult shouldBe Success(None)
+
+
+
+
+}
---
+++ playground/src/main/scala/ab/catz/OptionTInForApp.scala
@@ -0,0 +1,61 @@
+package ab.catz
+
+import cats.data._
+import cats.implicits._
+import org.scalatest.Matchers
+
+import scala.concurrent.{Await, Future}
+import scala.util.{Success, Try}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+
+/**
+  * For-comprehension example
+  */
+object OptionTInForApp extends App with Matchers {
+
+  val goodAsyncResp1 = OptionT(Future.successful(Option("201")))
+  val goodResp2 = Option("202")
+  val goodAsyncResp3 = OptionT(Future.successful(Option("203")))
+
+  /** all good */
+  val aggregatedResp: OptionT[Future, String] = for {
+    resp1 <- goodAsyncResp1
+    resp2 = goodResp2
+    resp3 <- goodAsyncResp3
+  } yield {
+    s"$resp1-$resp2-$resp3"
+  }
+
+  Await.result(aggregatedResp.value, 0 nanos) shouldBe Option("201-Some(202)-203")
+
+
+  /** inner is bad */
+  val badResp2 = Option.empty[String]
+
+  val aggregatedInnerBadResp = for {
+    resp1 <- goodAsyncResp1
+    resp2 = badResp2
+    resp3 <- goodAsyncResp3
+  } yield {
+    s"$resp1-$resp2-$resp3"
+  }
+
+  Await.result(aggregatedInnerBadResp.value, 0 nanos) shouldBe Option("201-None-203")
+
+  /** Outer is bad */
+  val badAsyncResp3 = OptionT(Future {
+    Option(throw new IllegalArgumentException("not good"))
+  })
+
+  val aggregatedOuterBadResp = for {
+    resp1 <- goodAsyncResp1
+    resp2 = goodResp2
+    resp3 <- badAsyncResp3
+  } yield {
+    s"$resp1-$resp2-$resp3"
+  }
+
+  aggregatedOuterBadResp.value.isCompleted shouldBe false
+
+}
